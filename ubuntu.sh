f6f12a1d9449d1976095b889838808efa7c0f67a
Oliver Steele <steele@osteele.com>
2016-01-21 11:37:02 -0500

    add ST3 too

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -eu
 # Usage: curl https://raw.githubusercontent.com/sd16spring/setup/master/ubuntu.sh | bash
 
-# Add the Atom (text editor)
+# Add the Atom (text editor) and Sublime Text 3 (ST3) (another text editor) repositories
 sudo add-apt-repository ppa:webupd8team/atom
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
 # Update and upgrade your system:
 sudo apt-get update
@@ -23,8 +24,9 @@ sudo pip install path.py markupsafe jupyter singledispatch backports_abc certifi
 # Install Allen's Python package swampy.
 sudo pip install swampy
 
-# Install Atom, and Atom's package manager
+# Install Atom and ST3
 sudo apt-get install -y atom
+sudo apt-get install sublime-text-installer
 
 # Install Atom packages
 apm install floobits
