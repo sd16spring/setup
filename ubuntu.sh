df1f5409c28b8b6be44b9da5247c3774e0db1d3a
Oliver Steele <steele@osteele.com>
2016-01-21 20:52:17 -0500

    echo the steps

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
 # Usage: curl https://raw.githubusercontent.com/sd16spring/setup/master/ubuntu.sh | bash
 
-# exit the script on errors (else bash continues to the next line)
-set -euo pipefail
+# Run the script strictly, for easier debugging and failure detection:
+set -e          # errors stop the script (instead of continuing to the next line)
+set -u          # fail when on undefined variable is read
+set -o pipefail # fail when the left side of a pipe fails
 
 # Add the Atom (text editor) and Sublime Text 3 (ST3) (another text editor) repositories
 sudo add-apt-repository ppa:webupd8team/atom
@@ -13,6 +15,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Git is the version control system we will use this semester.
+echo "Installing Git, and build essentials..."
 sudo apt-get install -y git
 sudo apt-get install -y build-essential
 
@@ -20,25 +23,27 @@ sudo apt-get install -y build-essential
 # - python-pip is a Python package installer.
 # - python-tk is a GUI toolkit for Python.
 # - python-dev is required for some Python packages to build
+echo "Installing Python..."
 sudo apt-get install -y python-pip python-tk python-dev
 
 # Install Python packages:
+echo "Installing Python packages..."
 sudo pip install flake8 # provides lint support to editor packages
 # Jupyter notebook, which you'll be using for pre-class exercises
 sudo pip install path.py markupsafe jupyter singledispatch backports_abc certifi jsonschema
 # Allen's Python package swampy.
 sudo pip install swampy
 
-# Install Atom and ST3
+# Install Atom and its packages
+echo "Installing Atom..."
 sudo apt-get install -y atom
-sudo apt-get install sublime-text-installer
-
-# Install Atom packages
 apm install floobits
 apm install linter
 apm install linter-flake8
 
-# Install Sublime Text 3 package manager and packages
+# Install Sublime Text 3, Package Control, and packages
+echo "Installing Sublime Text 3..."
+sudo apt-get install sublime-text-installer
 curl https://sublime.wbond.net/Package%20Control.sublime-package -o ~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package
 ST3_CONFIG_DIR=~/.config/sublime-text-3
 ST3_USER_SETTINGS=$ST3_CONFIG_DIR/Packages/User/Package\ Control.sublime-settings
